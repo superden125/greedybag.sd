0caa474cf28143056a5c02c82435cb5a753900c6
sd <superden125@gmail.comgit config --global user.name superden125>
2020-10-16 22:34:43 +0700

    Updates

--- precache-manifest.99aa9792d37ed6e060c28fee3a7638df.js
+++ precache-manifest.99aa9792d37ed6e060c28fee3a7638df.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6549a76b700f960220bf2f338b96c021",
+    "revision": "a572eee98607ad16bb071e44602a78bd",
     "url": "/greedybag.sd/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/greedybag.sd/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "b0a3ac039080255926eb",
-    "url": "/greedybag.sd/static/css/main.d1b23f4a.chunk.css"
+    "revision": "530d03e877496ac47353",
+    "url": "/greedybag.sd/static/css/main.d81d85d1.chunk.css"
   },
   {
     "revision": "8222f80da547c963359c",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/greedybag.sd/static/js/2.03f901cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0a3ac039080255926eb",
-    "url": "/greedybag.sd/static/js/main.6aeee769.chunk.js"
+    "revision": "530d03e877496ac47353",
+    "url": "/greedybag.sd/static/js/main.bee5b47f.chunk.js"
   },
   {
     "revision": "889860abe18ed1d85bb3",
